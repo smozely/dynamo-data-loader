ef893ff77e544ccd515635b3851869c64ebf0e00
smozely <me@steve-mosley.com>
2015-10-11 10:45:49 +1300

    FEAT: Adding load to the API

--- src/main/java/com/smozely/dynamoloader/DynamoDBDataLoader.java
+++ src/main/java/com/smozely/dynamoloader/DynamoDBDataLoader.java
@@ -2,10 +2,14 @@ package com.smozely.dynamoloader;
 
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.smozely.dynamoloader.internal.ClearOperation;
 import com.smozely.dynamoloader.internal.DataLoaderOperation;
+import com.smozely.dynamoloader.internal.LoadOperation;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class DynamoDBDataLoader {
@@ -13,6 +17,8 @@ public class DynamoDBDataLoader {
     private final DynamoDB dynamo;
 
     private final List<DataLoaderOperation> operations = Lists.newLinkedList();
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public static DynamoDBDataLoader with(DynamoDB dynamo) {
         return new DynamoDBDataLoader(dynamo);
@@ -31,6 +37,18 @@ public class DynamoDBDataLoader {
         return this;
     }
 
+    public DynamoDBDataLoader load(String tableName, File jsonFile) {
+        return load(dynamo.getTable(tableName), jsonFile);
+    }
+
+    public DynamoDBDataLoader load(Table table, File jsonFile) {
+        try {
+            operations.add(new LoadOperation(table, mapper.readTree(jsonFile)));
+        } catch (IOException e) {
+            throw new RuntimeException("IO Exception reading jsonFile", e);
+        }
+        return this;
+    }
 
     public void execute() {
         operations.forEach(DataLoaderOperation::execute);
